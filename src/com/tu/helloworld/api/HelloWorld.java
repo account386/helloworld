c2e3a62f82383c9850d412170f1cce006d3ced5f
Veselin Marinov <veselin.marinov@egt-interactive.com>
2018-03-06 11:45:32 +0200

    Init commit

---
+++ src/com/tu/helloworld/api/HelloWorld.java
@@ -0,0 +1,24 @@
+package com.tu.helloworld.api;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloWorld
+ */
+@WebServlet("/")
+public class HelloWorld extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public HelloWorld() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Bye Bye!!");
+	}
+}
